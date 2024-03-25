2ecb8b49ddfe84185007bda2cef20cb9e965802b
StevenOctoberFirst <129754951+StevenOctoberFirst@users.noreply.github.com>
2024-03-25 13:51:55 -0400

    Added Popups and the Attack Decision

--- Assets/Scripts/Enemies/FSM/Decisions/Decision_AttackRange.cs
+++ Assets/Scripts/Enemies/FSM/Decisions/Decision_AttackRange.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Decision_AttackRange : MonoBehaviour
+public class Decision_AttackRange : FSM_Decision
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float attackRange;
+    [SerializeField] LayerMask playerLayer;
+
+    EnemyBrain enemyBrain;
+
+    private void Awake()
     {
-        
+        enemyBrain = GetComponent<EnemyBrain>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool Decide()
     {
-        
+        return InAttackRange();
+    }
+
+    private bool InAttackRange()
+    {
+        if (enemyBrain.Player == null)
+            return false;
+
+        Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, attackRange, playerLayer);
+
+        if (playerCollider != null)
+            return true;
+
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
